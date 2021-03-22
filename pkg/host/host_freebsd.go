88ea6b56ea38537f6c3731ec085e63e86f4933ee
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2021-03-22 18:31:40 +0100

    Disable unsupported feature comparisons for FreeBSD in breakpoint mode

--- pkg/host/host_freebsd.go
+++ pkg/host/host_freebsd.go
@@ -13,6 +13,6 @@ func isSupported(c *prog.Syscall, target *prog.Target, sandbox string) (bool, st
 
 func init() {
 	checkFeature[FeatureCoverage] = unconditionallyEnabled
-	checkFeature[FeatureComparisons] = unconditionallyEnabled
+	// checkFeature[FeatureComparisons] = unconditionallyEnabled
 	checkFeature[FeatureNetInjection] = unconditionallyEnabled
 }
